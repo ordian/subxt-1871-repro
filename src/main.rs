c28f6f7c54ee3a8e68e8b94ec780a56f850ac81c
ordian <noreply@reusable.software>
2024-11-18 18:13:39 +0100

    simplify

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use subxt::{OnlineClient, PolkadotConfig, dynamic::{tx, Value}};
-use subxt_signer::{sr25519, DEV_PHRASE, SecretUri};
-use std::str::FromStr;
+use subxt_signer::sr25519::dev;
 
 #[subxt::subxt(runtime_metadata_path = "./artifacts/kusama.scale")]
 pub mod kusama {}
@@ -10,9 +9,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let api =
         OnlineClient::<PolkadotConfig>::from_url("wss://kusama-rpc.polkadot.io").await?;
 
-    let derivation = format!("{DEV_PHRASE}//getrandom-please");
-    let uri = SecretUri::from_str(&derivation).unwrap();
-    let signer = sr25519::Keypair::from_uri(&uri).unwrap();
+    let signer = dev::alice();
 
     let tx_payload = tx("System", "remark", vec![Value::from_bytes("getrandom_or_die")]);
 
